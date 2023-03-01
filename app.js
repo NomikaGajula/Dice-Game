1fdeb439d1d4419f49f98378f4ca5ad366a83f55
Nomika <nomigajula412@gmail.com>
2023-03-01 21:59:06 +0530

    added gitignore

---
+++ app.js
@@ -0,0 +1,52 @@
+const express=require("express");
+const app=express();
+const bodyParser=require("body-parser");
+const https=require("https");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("hello"));
+app.get("/",function(req,res){
+	res.sendFile(__dirname+"/signup.html");
+});
+app.post("/",function (req,res) {
+	const fname=req.body.fname;
+	const lname=req.body.lname;
+	const email=req.body.email;
+	const data={
+		members:[
+             {
+             	email_address:email,
+             	status:"subscribed",
+             	merge_fields:{
+             		FNAME:fname,
+             		LNAME:lname
+             	}
+             }
+			]
+	};
+	const jsonData=JSON.stringify(data);
+	const url="https://us13.api.mailchimp.com/3.0/lists/8ec9ab551d"
+   const options={
+   	method:"POST",
+   	auth:"angela:202ce00abc9179821b37703bf6d4713c-us13"
+   }
+   const request=https.request(url,options,function(response){
+   	if(response.statusCode===200){
+   		res.send("Success");
+   	}else {
+   		res.send("not success");
+   	}
+      response.on("data",function(data){
+      	console.log(JSON.parse(data));
+      })
+   })
+   request.write(jsonData);
+   request.end()
+});
+app.listen(4000,function(){
+	console.log("The server is started");
+});
+
+// apikey
+// 202ce00abc9179821b37703bf6d4713c-us13
+// listid
+// 8ec9ab551d
